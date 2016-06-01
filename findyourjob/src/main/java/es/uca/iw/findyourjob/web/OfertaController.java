2af46c5c8733951c23ef978178e4bf55d4465a8c
lauraag <laura.alvarez.gallego@gmail.com>
2016-06-01 16:57:53 +0200

    Listar ofertas - SIN ROLES

--- findyourjob/src/main/java/es/uca/iw/findyourjob/web/OfertaController.java
+++ findyourjob/src/main/java/es/uca/iw/findyourjob/web/OfertaController.java
@@ -1,9 +1,16 @@
 package es.uca.iw.findyourjob.web;
+import es.uca.iw.findyourjob.domain.Empresa;
 import es.uca.iw.findyourjob.domain.Oferta;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 import org.gvnix.addon.web.mvc.annotations.jquery.GvNIXWebJQuery;
 import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 
@@ -17,5 +24,20 @@ public class OfertaController {
     @RequestMapping(params = "find=candidatos", method = RequestMethod.GET)
     public String buscarCandidatos() {
         return "hola";
+    }
+    
+
+	@RequestMapping(produces = "text/html")
+    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute("ofertas", Oferta.findOfertaEntries(firstResult, sizeNo, sortFieldName, sortOrder));
+            float nrOfPages = (float) Oferta.countOfertas() / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("ofertas", Oferta.findAllOfertas(sortFieldName, sortOrder));
+        }
+        return "ofertas/list";
     }
 }
